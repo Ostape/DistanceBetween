f71e4b16efe9ffbfe3d064f2cffc220ed2e9baba
Orest Shemeliuk <orestshemelyuk@gmail.com>
2020-09-25 00:34:31 +0300

    connect Realtime Database to application

--- app/src/main/java/com/robosh/distancebetween/MainActivity.kt
+++ app/src/main/java/com/robosh/distancebetween/MainActivity.kt
@@ -2,10 +2,24 @@ package com.robosh.distancebetween
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rootNode: FirebaseDatabase
+    private lateinit var reference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mButton.setOnClickListener {
+            rootNode = FirebaseDatabase.getInstance()
+            reference = rootNode.getReference("Users")
+
+            reference.setValue("Second data storage")
+        }
     }
 }
