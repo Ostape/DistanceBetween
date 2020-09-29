c7c3dc350c62c1875f822d52aa65143475ece066
Orest Shemeliuk <orestshemelyuk@gmail.com>
2020-09-29 20:34:06 +0300

    add permission check for getting location

--- app/src/main/java/com/robosh/distancebetween/MainActivity.kt
+++ app/src/main/java/com/robosh/distancebetween/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.robosh.distancebetween
 
+import android.Manifest
+import android.content.DialogInterface
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private companion object {
+        const val ACCESS_FINE_LOCATION_PERMISSION_CODE = 20
+    }
 
     private lateinit var rootNode: FirebaseDatabase
     private lateinit var reference: DatabaseReference
@@ -15,11 +26,69 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mButton.setOnClickListener {
-            rootNode = FirebaseDatabase.getInstance()
-            reference = rootNode.getReference("Users")
+        //            rootNode = FirebaseDatabase.getInstance()
+//            reference = rootNode.getReference("Users")
+//
+//            reference.setValue("Second data storage")
+        requestPermissionBtn.setOnClickListener {
+            checkForAccessLocationPermission()
+        }
+    }
 
-            reference.setValue("Second data storage")
+    private fun checkForAccessLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            Toast.makeText(this, "You have already have permission", Toast.LENGTH_SHORT).show()
+        } else {
+            requestAccessLocationPermission()
+        }
+    }
+
+    private fun requestAccessLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            AlertDialog.Builder(this)
+                .setTitle("Permission needed Title")
+                .setMessage("I need this permission Message")
+                .setPositiveButton("Ok") { dialog, which ->
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        ACCESS_FINE_LOCATION_PERMISSION_CODE
+                    )
+                }
+                .setNegativeButton("cancel") { dialog, which ->
+                    dialog.dismiss()
+                }
+                .create()
+                .show()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                ACCESS_FINE_LOCATION_PERMISSION_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == ACCESS_FINE_LOCATION_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
