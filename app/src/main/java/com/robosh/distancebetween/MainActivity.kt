853971cc41fbf361af5a99820aa8dd2fdb386a0e
Orest Shemeliuk <orestshemelyuk@gmail.com>
2020-09-30 23:05:59 +0300

    create binding with service and create notification for foreground service using

--- app/src/main/java/com/robosh/distancebetween/MainActivity.kt
+++ app/src/main/java/com/robosh/distancebetween/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.robosh.distancebetween
 
 import android.Manifest
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.robosh.distancebetween.locationservice.ForegroundLocationService
 import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
 
@@ -22,6 +28,25 @@ class MainActivity : AppCompatActivity() {
     private lateinit var rootNode: FirebaseDatabase
     private lateinit var reference: DatabaseReference
 
+    private var foregroundOnlyLocationServiceBound = false
+
+    // Provides location updates for while-in-use feature.
+    private var foregroundOnlyLocationService: ForegroundLocationService? = null
+
+    private val foregroundOnlyServiceConnection = object : ServiceConnection {
+
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            val binder = service as ForegroundLocationService.LocalBinder
+            foregroundOnlyLocationService = binder.service
+            foregroundOnlyLocationServiceBound = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            foregroundOnlyLocationService = null
+            foregroundOnlyLocationServiceBound = false
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,17 +55,36 @@ class MainActivity : AppCompatActivity() {
 //            reference = rootNode.getReference("Users")
 //
 //            reference.setValue("Second data storage")
-        requestPermissionBtn.setOnClickListener {
+        receiveLocationUpdatesBtn.setOnClickListener {
             checkForAccessLocationPermission()
+        }
+        stopBtn.setOnClickListener {
+            foregroundOnlyLocationService?.onUnSubscribe()
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+        val serviceIntent = Intent(this, ForegroundLocationService::class.java)
+        bindService(serviceIntent, foregroundOnlyServiceConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (foregroundOnlyLocationServiceBound) {
+            unbindService(foregroundOnlyServiceConnection)
+            foregroundOnlyLocationServiceBound = false
+        }
+    }
+
+    private fun isPermissionGranted(): Boolean = ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
     private fun checkForAccessLocationPermission() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
+        if (isPermissionGranted()) {
+            foregroundOnlyLocationService?.onSubscribe()
             Timber.d("You have already have permission")
             Toast.makeText(this, "You have already have permission", Toast.LENGTH_SHORT).show()
         } else {
@@ -59,6 +103,7 @@ class MainActivity : AppCompatActivity() {
                 .setMessage("I need this permission Message")
                 .setPositiveButton("Ok") { dialog, which ->
                     Timber.d("Request permission from Dialog")
+                    foregroundOnlyLocationService?.onSubscribe()
                     ActivityCompat.requestPermissions(
                         this,
                         arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
@@ -73,6 +118,7 @@ class MainActivity : AppCompatActivity() {
                 .show()
         } else {
             Timber.d("Request permission")
+            foregroundOnlyLocationService?.onSubscribe()
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
--- app/src/main/java/com/robosh/distancebetween/locationservice/ForegroundLocationService.kt
+++ app/src/main/java/com/robosh/distancebetween/locationservice/ForegroundLocationService.kt
@@ -1,16 +1,132 @@
 package com.robosh.distancebetween.locationservice
 
-import android.app.Service
+import android.app.*
+import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.robosh.distancebetween.MainActivity
+import com.robosh.distancebetween.R
+import timber.log.Timber
 
 class ForegroundLocationService : Service() {
 
     private val localBinder: Binder = LocalBinder()
+    private var configurationChange = false
+
+
+    private lateinit var notificationManager: NotificationManager
+
+    private companion object {
+        const val NOTIFICATION_CHANNEL_ID = "NOTIFICATION_CHANNEL_ID"
+        const val NOTIFICATION_ID = 110
+
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.d("Service onCreate Callback")
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Timber.d("Service onStartCommand Callback")
+        return START_NOT_STICKY
+    }
 
     override fun onBind(intent: Intent?): IBinder? {
+        Timber.d("Service onBind method")
+
+        stopForeground(true)
+        configurationChange = false
         return localBinder
+    }
+
+    override fun onRebind(intent: Intent) {
+        Timber.d("Service onRebind method")
+
+        stopForeground(true)
+//        serviceRunningInForeground = false
+        configurationChange = false
+        super.onRebind(intent)
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        Timber.d("Service onUnbind method")
+
+        if (configurationChange.not()) {
+            val notification = createNotification()
+            startForeground(NOTIFICATION_ID, notification)
+        }
+        return true
+    }
+
+    fun onSubscribe() {
+        Timber.d("OnSubscribe to location changes")
+        startService(Intent(applicationContext, ForegroundLocationService::class.java))
+    }
+
+    fun onUnSubscribe() {
+        Timber.d("onUnSubscribe from location changes")
+        stopSelf()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        configurationChange = true
+    }
+
+
+    private fun createNotification(): Notification {
+        Timber.d("Creating Notification")
+        val titleText = "Title notification"
+        val mainNotificationText = "Notification message"
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID, titleText, NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        val bigTextStyle = NotificationCompat.BigTextStyle()
+            .bigText(mainNotificationText)
+            .setBigContentTitle(titleText)
+
+        val launchActivityIntent = Intent(this, MainActivity::class.java)
+
+//        val cancelIntent = Intent(this, ForegroundLocationService::class.java)
+//        cancelIntent.putExtra(EXTRA_CANCEL_LOCATION_TRACKING_FROM_NOTIFICATION, true)
+
+        val activityPendingIntent = PendingIntent.getActivity(
+            this, 0, launchActivityIntent, 0
+        )
+
+        val notificationCompatBuilder =
+            NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID)
+
+        return notificationCompatBuilder
+            .setStyle(bigTextStyle)
+            .setContentTitle(titleText)
+            .setContentText(mainNotificationText)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setOngoing(true)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .addAction(
+                R.drawable.ic_launcher_foreground, "Launch Activity",
+                activityPendingIntent
+            )
+//            .addAction(
+//                R.drawable.ic_cancel,
+//                "Stop service",
+//                servicePendingIntent
+//            )
+            .build()
     }
 
     inner class LocalBinder : Binder() {
