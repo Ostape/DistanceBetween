b3bdb65b87c78e9776cbf769ec323f4172e74e78
Orest Shemeliuk <orestshemelyuk@gmail.com>
2020-11-08 02:33:34 +0200

    move request location permission to fragment

--- app/src/main/java/com/robosh/distancebetween/MainActivity.kt
+++ app/src/main/java/com/robosh/distancebetween/MainActivity.kt
@@ -1,38 +1,20 @@
 package com.robosh.distancebetween
 
-import android.Manifest
-import android.appwidget.AppWidgetManager
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.IBinder
-import android.widget.RemoteViews
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.robosh.distancebetween.locationservice.ForegroundLocationService
-import com.robosh.distancebetween.widget.LocationWidgetProvider
-import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
-
-    private companion object {
-        const val ACCESS_FINE_LOCATION_PERMISSION_CODE = 20
-    }
 
     private var foregroundOnlyLocationServiceBound = false
 
     // Provides location updates for while-in-use feature.
     private var foregroundOnlyLocationService: ForegroundLocationService? = null
-
-    private var isReceivingLocationUpdates: Boolean = false
-
-    private var testInt = 0
 
     private val foregroundOnlyServiceConnection = object : ServiceConnection {
 
@@ -51,42 +33,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val fragmentManager = supportFragmentManager
-//        val fragmentTransaction = fragmentManager.beginTransaction()
-//        fragmentTransaction.add(R.id.fragmentContainer, SaveUserFragment.newInstance())
-//        fragmentTransaction.commit()
-
-//
-//        val sharedPref =
-//            getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
-//        isReceivingLocationUpdates =
-//            sharedPref.getBoolean(getString(R.string.is_receiving_location_updates), false)
-////
-//        if (isReceivingLocationUpdates) {
-//            receiveLocationUpdatesBtn.text = getString(R.string.stop_receiving_location)
-//        }
-//
-//        receiveLocationUpdatesBtn.setOnClickListener {
-//            val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
-//
-//            // TODO add stop service when GPS is disabled
-//            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-//                if (isReceivingLocationUpdates) {
-//                    onStopReceiveLocationUpdates()
-//                } else {
-//                    checkForAccessLocationPermission()
-//                }
-//            } else {
-//                Toast.makeText(this, "Enable GPS!!!", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//        sendBroadcast.setOnClickListener {
-//            sendWidgetBroadcast()
-//        }
-//        addNewUser.setOnClickListener {
-//            RealtimeDatabaseRepository.newInstance()
-//                .saveUser(User(name = "Petro", surname = "Poroshenko"))
-//        }
     }
 
     override fun onStart() {
@@ -103,113 +49,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == ACCESS_FINE_LOCATION_PERMISSION_CODE) {
-            if (grantResults.isNotEmpty() && grantResults.first() == PackageManager.PERMISSION_GRANTED) {
-                Timber.d("Permission GRANTED")
-                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show()
-//                onStartReceiveLocationUpdates()
-            } else {
-                Timber.d("Permission DENIED")
-                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
 
-    private fun checkForAccessLocationPermission() {
-        if (isPermissionGranted()) {
-//            onStartReceiveLocationUpdates()
-            Timber.d("You have already have permission")
-            Toast.makeText(this, "You have already have permission", Toast.LENGTH_SHORT).show()
-        } else {
-            requestAccessLocationPermission()
-        }
-    }
-
-    private fun isPermissionGranted(): Boolean = ContextCompat.checkSelfPermission(
-        this,
-        Manifest.permission.ACCESS_FINE_LOCATION
-    ) == PackageManager.PERMISSION_GRANTED
-
-    private fun requestAccessLocationPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            )
-        ) {
-            // todo replace with DialogFragment
-            AlertDialog.Builder(this)
-                // todo extract to strings
-                .setTitle("Permission needed Title")
-                .setMessage("I need this permission Message")
-                .setPositiveButton("Ok") { _, _ ->
-                    Timber.d("Request permission from Dialog")
-                    ActivityCompat.requestPermissions(
-                        this,
-                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                        ACCESS_FINE_LOCATION_PERMISSION_CODE
-                    )
-                }
-                .setNegativeButton("cancel") { dialog, _ ->
-                    Timber.d("Cancel permission Dialog")
-                    dialog.dismiss()
-                }
-                .create()
-                .show()
-        } else {
-            Timber.d("Request permission")
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                ACCESS_FINE_LOCATION_PERMISSION_CODE
-            )
-        }
-    }
 //
 //    private fun onStopReceiveLocationUpdates() {
 //        foregroundOnlyLocationService?.onUnSubscribe()
-//        receiveLocationUpdatesBtn.text = getString(R.string.start_receiving_location)
-//        isReceivingLocationUpdates = false
-//        val sharedPref =
-//            getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
-//        sharedPref.edit().putBoolean(
-//            getString(R.string.is_receiving_location_updates),
-//            isReceivingLocationUpdates
-//        ).apply()
 //    }
 //
 //    private fun onStartReceiveLocationUpdates() {
 //        foregroundOnlyLocationService?.onSubscribe()
-//        receiveLocationUpdatesBtn.text = getString(R.string.stop_receiving_location)
-//        isReceivingLocationUpdates = true
-//        // todo extract to extensions
-//        val sharedPref =
-//            getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
-//        sharedPref.edit().putBoolean(
-//            getString(R.string.is_receiving_location_updates),
-//            isReceivingLocationUpdates
-//        ).apply()
 //    }
 
-    private fun sendWidgetBroadcast() {
-        val thisWidget = ComponentName(applicationContext, LocationWidgetProvider::class.java)
-        val appWidgetManager = AppWidgetManager.getInstance(this.applicationContext)
-        val allWidgetIds: IntArray = appWidgetManager.getAppWidgetIds(thisWidget)
-
-        for (widgetId in allWidgetIds) {
-            // create some random data
-            val remoteViews =
-                RemoteViews(this.applicationContext.packageName, R.layout.widget_location)
-            // Set the text
-            remoteViews.setTextViewText(R.id.exampleWidgetButton, "Random: $testInt")
-            testInt++
-            // Register an onClickListener
-            appWidgetManager.updateAppWidget(widgetId, remoteViews)
-        }
-    }
+    //////////////////////////////////////////////
+//    private fun sendWidgetBroadcast() {
+//        val thisWidget = ComponentName(applicationContext, LocationWidgetProvider::class.java)
+//        val appWidgetManager = AppWidgetManager.getInstance(this.applicationContext)
+//        val allWidgetIds: IntArray = appWidgetManager.getAppWidgetIds(thisWidget)
+//
+//        for (widgetId in allWidgetIds) {
+//            // create some random data
+//            val remoteViews =
+//                RemoteViews(this.applicationContext.packageName, R.layout.widget_location)
+//            // Set the text
+//            remoteViews.setTextViewText(R.id.exampleWidgetButton, "Random: $testInt")
+//            testInt++
+//            // Register an onClickListener
+//            appWidgetManager.updateAppWidget(widgetId, remoteViews)
+//        }
+//    }
 }
--- app/src/main/java/com/robosh/distancebetween/homescreen/view/HomeScreenFragment.kt
+++ app/src/main/java/com/robosh/distancebetween/homescreen/view/HomeScreenFragment.kt
@@ -1,16 +1,33 @@
 package com.robosh.distancebetween.homescreen.view
 
+import android.Manifest
+import android.content.Context.LOCATION_SERVICE
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.robosh.distancebetween.application.INTENT_USERNAME
 import com.robosh.distancebetween.databinding.FragmentHomeScreenBinding
 import com.robosh.distancebetween.homescreen.viewmodel.HomeScreenViewModel
+import timber.log.Timber
+
 
 class HomeScreenFragment : Fragment() {
+
+    private companion object {
+        const val ACCESS_FINE_LOCATION_PERMISSION_CODE = 20
+    }
 
     private lateinit var binding: FragmentHomeScreenBinding
 
@@ -30,6 +47,91 @@ class HomeScreenFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val username = arguments?.getString(INTENT_USERNAME)
         binding.usernameTextView.text = username
+        binding.connectToFriendButton.setOnClickListener {
+            checkForAccessLocationPermission()
+        }
+        binding.waitForConnectionButton.setOnClickListener {
+            checkForAccessLocationPermission()
+        }
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == ACCESS_FINE_LOCATION_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults.first() == PackageManager.PERMISSION_GRANTED) {
+                Timber.d("Permission GRANTED")
+                showToast("Permission GRANTED")
+//                onStartReceiveLocationUpdates()
+            } else {
+                Timber.d("Permission DENIED")
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri: Uri = Uri.fromParts("package", activity?.packageName, null)
+                intent.data = uri
+                startActivityForResult(intent, ACCESS_FINE_LOCATION_PERMISSION_CODE)
+            }
+        }
+    }
+
+    private fun checkForAccessLocationPermission() {
+        val locationManager = activity?.getSystemService(LOCATION_SERVICE) as LocationManager
+
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+//            onStartReceiveLocationUpdates()
+                Timber.d("You have already have permission")
+                // todo extract to strings
+                showToast("You have already have permission")
+            } else {
+                requestAccessLocationPermission()
+            }
+        } else {
+            showToast("Enable GPS!!!")
+        }
+    }
+
+    private fun requestAccessLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            // todo replace with DialogFragment
+            AlertDialog.Builder(requireContext())
+                .setTitle("Permission needed Title")
+                .setMessage("I need this permission Message")
+                .setPositiveButton("Ok") { _, _ ->
+                    Timber.d("Request permission from Dialog")
+                    ActivityCompat.requestPermissions(
+                        requireActivity(),
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        ACCESS_FINE_LOCATION_PERMISSION_CODE
+                    )
+                }
+                .setNegativeButton("cancel") { dialog, _ ->
+                    Timber.d("Cancel permission Dialog")
+                    dialog.dismiss()
+                }
+                .create()
+                .show()
+        } else {
+            Timber.d("Request permission")
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                ACCESS_FINE_LOCATION_PERMISSION_CODE
+            )
+        }
+    }
+
+    private fun showToast(text: String) {
+        Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show()
+    }
 }
