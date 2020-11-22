1f34def9fa84ce755c43d37de9e67f1e2208ed0a
Orest Shemeliuk <orestshemelyuk@gmail.com>
2020-11-23 00:43:59 +0200

    code refactor in homeScreen fragment

--- app/src/main/java/com/robosh/distancebetween/homescreen/view/HomeScreenFragment.kt
+++ app/src/main/java/com/robosh/distancebetween/homescreen/view/HomeScreenFragment.kt
@@ -10,6 +10,8 @@ import android.os.Bundle
 import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -21,8 +23,6 @@ import com.robosh.distancebetween.R
 import com.robosh.distancebetween.application.ACCESS_FINE_LOCATION_PERMISSION_CODE
 import com.robosh.distancebetween.application.INTENT_USERNAME
 import com.robosh.distancebetween.databinding.FragmentHomeScreenBinding
-import com.robosh.distancebetween.locationservice.ForegroundLocationService
-import timber.log.Timber
 
 
 class HomeScreenFragment : Fragment() {
@@ -42,15 +42,9 @@ class HomeScreenFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val username = arguments?.getString(INTENT_USERNAME)
         binding.usernameTextView.text = username
-        binding.connectToFriendButton.setOnClickListener {
+        initNavigationButtons()
+        binding.requestPermissionButton.setOnClickListener {
             checkForAccessLocationPermission()
-//            sendCommandService(ACTION_START_OR_RESUME_SERVICE)
-            findNavController().navigate(R.id.action_homeScreenFragment_to_connectToFriendFragment)
-        }
-        binding.waitForConnectionButton.setOnClickListener {
-            checkForAccessLocationPermission()
-//            sendCommandService(ACTION_STOP_SERVICE)
-            findNavController().navigate(R.id.action_homeScreenFragment_to_waitForFriendFragment)
         }
     }
 
@@ -62,10 +56,11 @@ class HomeScreenFragment : Fragment() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == ACCESS_FINE_LOCATION_PERMISSION_CODE) {
             if (grantResults.isNotEmpty() && grantResults.first() == PackageManager.PERMISSION_GRANTED) {
-                Timber.d("Permission GRANTED")
                 showToast("Permission GRANTED")
+                binding.waitForConnectionButton.visibility = VISIBLE
+                binding.connectToFriendButton.visibility = VISIBLE
+                binding.requestPermissionButton.visibility = GONE
             } else {
-                Timber.d("Permission DENIED")
                 val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
                 val uri: Uri = Uri.fromParts("package", activity?.packageName, null)
                 intent.data = uri
@@ -75,21 +70,10 @@ class HomeScreenFragment : Fragment() {
     }
 
     private fun checkForAccessLocationPermission() {
-        val locationManager = activity?.getSystemService(LOCATION_SERVICE) as LocationManager
-
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            if (ContextCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                )
-                == PackageManager.PERMISSION_GRANTED
-            ) {
-                showToast("You have already have permission")
-            } else {
-                requestAccessLocationPermission()
-            }
+        if (isLocationPermissionGranted()) {
+            showToast("You have already have permission")
         } else {
-            showToast("Enable GPS!!!")
+            requestAccessLocationPermission()
         }
     }
 
@@ -99,41 +83,63 @@ class HomeScreenFragment : Fragment() {
                 Manifest.permission.ACCESS_FINE_LOCATION
             )
         ) {
-            // todo replace with DialogFragment
             AlertDialog.Builder(requireContext())
                 .setTitle("Permission needed Title")
                 .setMessage("I need this permission Message")
                 .setPositiveButton("Ok") { _, _ ->
-                    Timber.d("Request permission from Dialog")
-                    ActivityCompat.requestPermissions(
-                        requireActivity(),
-                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                        ACCESS_FINE_LOCATION_PERMISSION_CODE
-                    )
+                    requestListOfPermissions()
                 }
                 .setNegativeButton("cancel") { dialog, _ ->
-                    Timber.d("Cancel permission Dialog")
                     dialog.dismiss()
                 }
                 .create()
                 .show()
         } else {
-            Timber.d("Request permission")
-            requestPermissions(
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                ACCESS_FINE_LOCATION_PERMISSION_CODE
-            )
+            requestListOfPermissions()
         }
     }
 
-    private fun sendCommandService(action: String) {
-        Intent(requireContext(), ForegroundLocationService::class.java).also {
-            it.action = action
-            requireContext().startService(it)
-        }
+    private fun isLocationPermissionGranted(): Boolean {
+        return (ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun requestListOfPermissions() {
+        requestPermissions(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
+            ACCESS_FINE_LOCATION_PERMISSION_CODE
+        )
     }
 
     private fun showToast(text: String) {
         Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun initNavigationButtons() {
+        binding.connectToFriendButton.setOnClickListener {
+            if (isEnabledGps()) {
+                findNavController().navigate(R.id.action_homeScreenFragment_to_connectToFriendFragment)
+            }
+        }
+        binding.waitForConnectionButton.setOnClickListener {
+            if (isEnabledGps()) {
+                findNavController().navigate(R.id.action_homeScreenFragment_to_waitForFriendFragment)
+            }
+        }
+    }
+
+    private fun isEnabledGps(): Boolean {
+        val locationManager = activity?.getSystemService(LOCATION_SERVICE) as LocationManager
+        return if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            true
+        } else {
+            showToast("Enable GPS!!!")
+            false
+        }
     }
 }
